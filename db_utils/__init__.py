33efe8dbec7bd201d4932bc22bdfc99ebaa938b0
onemodtwo <onemodtwo@gmail.com>
2018-05-01 13:46:26 -0400

    refine notebook

--- db_utils/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Provides utility to connect to PostgreSQL database and return cursor
-"""
-
-from sys import path
-path.append('./db_utils/')
-from connect import connect
