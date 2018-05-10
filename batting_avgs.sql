efad68c5975f2d601c36c93bc5d1ce4204cae299
onemodtwo <onemodtwo@gmail.com>
2018-05-10 18:07:47 -0400

    update in batting_avgs.sql

--- batting_avgs.sql
+++ batting_avgs.sql
@@ -2,19 +2,27 @@ DROP MATERIALIZED VIEW IF EXISTS BATTING_AVGS CASCADE;
 CREATE MATERIALIZED VIEW BATTING_AVGS AS
 WITH hits AS
 (
-  SELECT ATBATS.batter,count(*) AS num_hits
+  SELECT ATBATS.batter, count(*) AS num_hits
   FROM ATBATS
   WHERE ATBATS.ab_result = 'hit'
   GROUP BY ATBATS.batter
 ), outs AS
 (
-  SELECT ATBATS.batter,count(*) AS num_outs
+  SELECT ATBATS.batter, count(*) AS num_outs
   FROM ATBATS
   WHERE ATBATS.ab_result = 'out'
+  GROUP BY ATBATS.batter
+), no_ab AS
+(
+  SELECT ATBATS.batter, count(*) AS num_no_ab
+  FROM ATBATS
+  WHERE ATBATS.ab_result = 'no at-bat'
   GROUP BY ATBATS.batter
 )
 SELECT h.batter,
   1.0 * h.num_hits / (h.num_hits + o.num_outs) AS batting_avg,
-  h.num_hits + o.num_outs AS at_bats
+  h.num_hits + o.num_outs AS at_bats,
+  h.num_hits + o.num_outs + n.num_no_ab as plate_app
 FROM hits h JOIN outs o ON h.batter = o.batter
+  JOIN no_ab n on h.batter = n.batter
 ORDER BY (h.num_hits / (h.num_hits + o.num_outs)) DESC;
